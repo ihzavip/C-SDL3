e8d854602c72b63beb5b415fd22f0b50b069029e
ihza <ihzavip@gmail.com>
2026-02-23 22:30:34 +0700

    feat: tick

--- game/game.c
+++ game/game.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #include <termios.h>
+#include <time.h>
 #include <unistd.h>
 
 /*
@@ -12,6 +13,7 @@
   2. Player
   3. Enemy
   4. Interaction?
+  5.
 */
 
 struct termios orig_termios;
@@ -28,6 +30,8 @@ void enable_raw_mode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
+/* ------------------------------- */
+
 typedef struct {
   int x;  // max 20
   int y;  // max 20
@@ -37,6 +41,12 @@ typedef struct {
 Entity player = {5, 10, 10};
 Entity enemy = {5, 11, 10};
 char message[50] = "";
+
+time_t currentTime;
+struct tm *t;
+
+char date_str[20];
+char time_str[20];
 
 void render() {
   system("clear");
@@ -55,17 +65,35 @@ void render() {
     printf("\n");
   }
   printf("%s\n", message);
+
+  // TODO: change format for time to HH:mm:ss
+  strftime(date_str, sizeof(date_str), "%d-%m-%Y", t);
+  strftime(time_str, sizeof(time_str), "%H:%M:%S", t);
+  printf("Date: %s\n", date_str);
+  printf("Time: %s\n", time_str);
 }
 
 void app_init() {
+
+  // Set time
+  currentTime = time(NULL);
+  t = localtime(&currentTime);
+
   enable_raw_mode();
+  srand(time(NULL));
   render();
+}
+
+void game_tick() {
+  currentTime += 1;
+  t = localtime(&currentTime);
 }
 
 void app_update() {
 
   int input = getchar();
 
+  // EOF equal to 4 in ASCII
   if (input == EOF) {
     exit(0);
   }
@@ -78,6 +106,8 @@ void app_update() {
 
   int newX = player.x;
   int newY = player.y;
+
+  game_tick();
 
   if (input == 'k')
     newY--;
@@ -99,14 +129,20 @@ void app_update() {
     return;
   }
 
-  // TODO: Combat interaction
   if (newX == enemy.x && newY == enemy.y) {
-    enemy.hp -= 1;
 
-    // TODO: calculate combat
+    int random = rand() % 10;
 
-    snprintf(message, sizeof(message), "You hit an enemy! Enemy hp: %d",
-             enemy.hp);
+    // TODO: Combat interaction
+    if (random < 8) {
+      enemy.hp -= 1;
+      snprintf(message, sizeof(message), "You hit an enemy! Enemy hp: %d",
+               enemy.hp);
+    } else {
+      player.hp -= 1;
+      snprintf(message, sizeof(message), "Enemy hit you! Player hp: %d",
+               player.hp);
+    }
 
     if (enemy.hp <= 0) {
       enemy.x = -1;
@@ -114,6 +150,10 @@ void app_update() {
       strcpy(message, "Enemy has died");
     }
 
+    if (player.hp <= 0) {
+      strcpy(message, "You have died");
+      // TODO: set state game_over
+    }
     return;
   }
 
